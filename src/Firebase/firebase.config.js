609e5ee30db63b1b129ccef797d6ddb5eb0d4cbc
alaminislam34 <mi3548514@gmail.com>
2024-12-08 02:36:11 +0600

    env file issue solve and connect firebase

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.DATA_apiKey,
-  authDomain: import.meta.env.DATA_authDomain,
-  projectId: import.meta.env.DATA_projectId,
-  storageBucket: import.meta.env.DATA_storageBucket,
-  messagingSenderId: import.meta.env.DATA_messagingSenderId,
-  appId: import.meta.env.DATA_appId,
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
