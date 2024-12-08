39117cc7b916b6bb5d7558e1bac790d175cafcc7
alaminislam34 <mi3548514@gmail.com>
2024-12-08 23:38:30 +0600

    change 404 page image

--- src/Pages/InvalidPage.jsx
+++ src/Pages/InvalidPage.jsx
@@ -1,8 +1,12 @@
-import invalidPage from "../assets/404 Page/404.gif";
+import invalidPage from "../assets/404 Page/404page.jpg";
 const InvalidPage = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center">
-      <img src={invalidPage} alt="" />
+    <div className="w-full h-[80vh] p-6 md:p-12 flex justify-center items-center">
+      <img
+        className="w-9/12 sm:w-7/12 md:w-6/12 lg:w-5/12 mx-auto"
+        src={invalidPage}
+        alt=""
+      />
     </div>
   );
 };
